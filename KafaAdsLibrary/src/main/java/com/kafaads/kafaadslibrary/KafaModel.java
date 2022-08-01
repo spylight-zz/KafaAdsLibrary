17ea92f600b7f2bb1ac3da36541118efbab7b351
Cahyo Purnomo <cahyocool@gmail.com>
2022-08-01 16:41:11 +0700

    Initial commit

---
+++ KafaAdsLibrary/src/main/java/com/kafaads/kafaadslibrary/KafaModel.java
@@ -0,0 +1,85 @@
+package com.kafaads.kafaadslibrary;
+
+public class KafaModel {
+    String _status;
+    String _appid;
+    String _banner;
+    String _interstitial;
+    String _reward;
+    String _native;
+    String _openapp;
+    Boolean _app_suspend;
+    String _link_app_redirect;
+
+    public String get_status() {
+        return _status;
+    }
+
+    public void set_status(String _status) {
+        this._status = _status;
+    }
+
+    public String get_appid() {
+        return _appid;
+    }
+
+    public void set_appid(String _appid) {
+        this._appid = _appid;
+    }
+
+    public String get_banner() {
+        return _banner;
+    }
+
+    public void set_banner(String _banner) {
+        this._banner = _banner;
+    }
+
+    public String get_interstitial() {
+        return _interstitial;
+    }
+
+    public void set_interstitial(String _interstitial) {
+        this._interstitial = _interstitial;
+    }
+
+    public String get_reward() {
+        return _reward;
+    }
+
+    public void set_reward(String _reward) {
+        this._reward = _reward;
+    }
+
+    public String get_native() {
+        return _native;
+    }
+
+    public void set_native(String _native) {
+        this._native = _native;
+    }
+
+    public String get_openapp() {
+        return _openapp;
+    }
+
+    public void set_openapp(String _openapp) {
+        this._openapp = _openapp;
+    }
+
+    public Boolean get_app_suspend() {
+        return _app_suspend;
+    }
+
+    public void set_app_suspend(Boolean _app_suspend) {
+        this._app_suspend = _app_suspend;
+    }
+
+    public String get_link_app_redirect() {
+        return _link_app_redirect;
+    }
+
+    public void set_link_app_redirect(String _link_app_redirect) {
+        this._link_app_redirect = _link_app_redirect;
+    }
+}
